e3fb94b1af47011be290bc6ff529d1ee9989a8ed
SSebasthian <ssebasthian.perez@gmail.com>
2025-05-12 07:14:47 -0500

    Agrega el SDK de Firebase al proyecto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,29 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, getApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { provideHttpClient } from '@angular/common/http';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "cyber-sag", appId: "1:860011833534:web:095b25af7bd016a6e496ef", storageBucket: "cyber-sag.firebasestorage.app", apiKey: "AIzaSyCni7O4cZw6HIK_0tK2EXkmj665w5Kz7NU", authDomain: "cyber-sag.firebaseapp.com", messagingSenderId: "860011833534" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideStorage(() => getStorage())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()), 
+    provideFirebaseApp(() => 
+      initializeApp({ 
+        projectId: "cyber-sag", 
+        appId: "1:860011833534:web:095b25af7bd016a6e496ef", 
+        storageBucket: "cyber-sag.firebasestorage.app", 
+        apiKey: "AIzaSyCni7O4cZw6HIK_0tK2EXkmj665w5Kz7NU", 
+        authDomain: "cyber-sag.firebaseapp.com", 
+        messagingSenderId: "860011833534" 
+
+      })
+    ), 
+      provideAuth(() => getAuth()), 
+      provideFirestore(() => getFirestore()), 
+      provideStorage(() => getStorage())]
 };
