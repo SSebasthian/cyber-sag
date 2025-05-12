126372f77b54e069c603571128970e29919fc1c9
SSebasthian <ssebasthian.perez@gmail.com>
2025-05-12 09:36:44 -0500

    Funcion para mostrar la seccion activa en el sub-menu

--- src/app/pagina/phishing/phishing.component.ts
+++ src/app/pagina/phishing/phishing.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core'
 import {MatIconModule} from '@angular/material/icon'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-phishing',
   imports: [
-    MatIconModule
+    MatIconModule,
+    CommonModule
   ],
   templateUrl: './phishing.component.html',
   styleUrl: './phishing.component.css'
 })
 export class PhishingComponent {
 
+enlaceActivo: string = ''; //variable para almacenar ID de la Seccion Actual activa
+
+  secciones = [
+    '#por-que-section',
+    '#que-es-section',
+    '#como-se-ve',
+    '#trampas',
+    '#evitar-phishing'
+  ];
+
+  //Metodo que escucha el evento de desplazamiento scroll
+  @HostListener('window:scroll', [])
+  desplazamientoScroll() {
+    let encontrado = false; //variable que controla si hay seccion visible
+
+    for (let sectionId of this.secciones) {
+      const elemento = document.querySelector(sectionId);
+      if (elemento) {
+        const seccionActiva = elemento.getBoundingClientRect();
+        if (seccionActiva.top <= 100 && seccionActiva.bottom >= 100) {
+          this.enlaceActivo = sectionId;
+          encontrado = true;
+          break;
+        }
+      }
+    }
+
+    // Si no se encontró ninguna sección visible, limpiar activeLink
+    if (!encontrado) {
+      this.enlaceActivo = '';
+    }
+  }
 }
